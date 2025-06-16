e718eba301ef466c9c62f30bd67b0ecf49c0933a
MuhammadAbyaz <m.abyaz681@gmail.com>
2025-06-17 01:13:28 +0500

    sent response to client

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
@@ -10,20 +11,38 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+func handleConnection (conn net.Conn){
+	defer conn.Close()
+	for{
+	received := bytes.Buffer{}
+	buff := make([]byte, 1024)
+
+	_,err := conn.Read(buff)
+	if err != nil {
+		fmt.Println("Error reading buffer: ", err.Error())
+		break
+	}
+	received.Write(buff)
+	conn.Write([]byte{0,0,0,0,0,0,0,7})
+	}
+}
+
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
 }
